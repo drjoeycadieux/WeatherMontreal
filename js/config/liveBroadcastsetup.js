876c1dddefd867b7126425ae1662c1fde0a047d4
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-01-30 19:42:19 +0000

    inm.

--- js/config/liveBroadcastsetup.js
+++ js/config/liveBroadcastsetup.js
@@ -5,11 +5,27 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
+
 var player;
 function onYouTubePlayerAPIReady() {
-  player = new YT.Player('ytplayer', {
-    height: '360',
-    width: '640',
-    videoId: 'M7lc1UVf-VE'
+   q = '';
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet',
+    channelId: chaser.youtubeID,
+    maxResults: 1,
+    eventType: 'live',
+    type: 'video',
+    order: 'date'
   });
 }
+
+
+// var player;
+// function onYouTubePlayerAPIReady() {
+//   player = new YT.Player('ytplayer', {
+//     height: '360',
+//     width: '640',
+//     videoId: 'M7lc1UVf-VE'
+//   });
+// }
